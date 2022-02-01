5e941b400ef3ad4efd96bac7f983c99ff2bef605
Fernando Guerrero Espinosa <96fernando.guerrero@gmail.com>
2022-02-01 16:02:47 -0600

    Base XCode and Playground folder Added

---
+++ baseProject/baseProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  baseProject
+//
+//  Created by Fernando Guerrero Espinosa on 01/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
