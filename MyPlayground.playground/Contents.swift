ff9924267b4c23472bb50a4e47fc71485d5f902d
Fernando Guerrero Espinosa <96fernando.guerrero@gmail.com>
2022-02-09 16:58:45 -0600

    Section 3 Done!

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//constantes
+let stringconstante = "soy constante"
+
+//declaracion multiple de variables
+
+let miNombreCompleto = "Completo", miFecha = "15 Agosto",
+    ciudad = "CDMX", congelacionAgua = 0
+
+//Asignacion Implicita y Explicita
+
+//EXPLICITA, estamos forzando a que solo sea tipo entera:
+var num: Int = 20
+
+//Implicita
+var num2 = 20 
+
+
